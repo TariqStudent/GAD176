e4b206df0b1eb15710575db06fd1df1495045b97
TariqStudent <159271240+TariqStudent@users.noreply.github.com>
2024-03-02 21:45:19 +0400

    Feature - Main Menu
    added instructions on the build setting and how it should be before playing the game, using commentss

--- Scripts/MainMenu1.cs
+++ Scripts/MainMenu1.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+//build setting should be the main menu first, and project 1 next.
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
