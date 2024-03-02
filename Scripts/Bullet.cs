50c8f876b6f572784f92306abad8ccf370d1e1c7
TariqStudent <159271240+TariqStudent@users.noreply.github.com>
2024-03-02 19:58:19 +0400

    added comments on Bullet.cs
    Added several comments on each section of this script so that it has instructions on what each and every code does in this script.

--- Scripts/Bullet.cs
+++ Scripts/Bullet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-// Added a comment regaring this protected float speed and the rigidboyd2d bulletRB.
+ // Added a comment regaring this protected float speed and the rigidboyd2d bulletRB.
     GameObject target;
     [SerializeField] protected float speed;
     Rigidbody2D bulletRB;
@@ -17,12 +17,12 @@ public class Bullet : MonoBehaviour
 
         //bullet script, it technically has an indirect subtraction of vectors, applied to the bullet prefab. 
         bulletRB = GetComponent<Rigidbody2D>();
-        target = GameObject.FindGameObjectWithTag("Player"); //all the target will be the player which is the slime.
+        target = GameObject.FindGameObjectWithTag("Player");   //all the target will be the player which is the slime.
         Vector2 moveDir = (target.transform.position - transform.position).normalized * speed; // <-- Normalisation.
         bulletRB.velocity = new Vector2 (moveDir.x, moveDir.y);
         Destroy(this.gameObject, 2);
 
-// check for null if not returns refernece and it will print on console as error.
+ // check for null if not returns refernece and it will print on console as error.
         if (target != null)
         {
             Debug.Log("Error");
